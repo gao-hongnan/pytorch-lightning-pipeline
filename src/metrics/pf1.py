1c5c04fb6049cc564545bc02e68df87a3e76b753
gaohn <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2023-02-18 12:24:30 +0800

    refactor: clean up

--- src/metrics/pf1.py
+++ src/metrics/pf1.py
@@ -1,11 +1,47 @@
+"""Probabilistic F1 score for binary classification."""
 import torch
 from torchmetrics import Metric
 import numpy as np
 from typing import Tuple
 
 
-class BinaryProbF1(Metric):
-    pass
+import torch
+from torchmetrics import Metric
+
+
+class BinaryPFBeta(Metric):
+    def __init__(self, beta: float = 1, dist_sync_on_step=False):
+        super().__init__(dist_sync_on_step=dist_sync_on_step)
+        self.beta = beta
+        self.add_state("ctp", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("cfp", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("y_true_count", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor):
+        if preds.ndim == 2:
+            preds = preds[:, 1]
+        preds = preds.clip(0, 1)
+        self.y_true_count += target.sum()
+        self.ctp += preds[target == 1].sum()
+        self.cfp += preds[target == 0].sum()
+
+    def compute(self):
+        c_precision = self.ctp / (self.ctp + self.cfp)
+        c_recall = self.ctp / self.y_true_count
+        beta_squared = self.beta ** 2
+
+        if c_precision > 0 and c_recall > 0:
+            return (
+                (1 + beta_squared)
+                * (c_precision * c_recall)
+                / (beta_squared * c_precision + c_recall)
+            ).item()
+        return 0.0
+
+    def reset(self):
+        self.ctp = torch.tensor(0)
+        self.cfp = torch.tensor(0)
+        self.y_true_count = torch.tensor(0)
 
 
 def pfbeta_torch(preds: torch.Tensor, labels: torch.Tensor, beta: float = 1) -> float:
@@ -31,7 +67,6 @@ def pfbeta_torch(preds: torch.Tensor, labels: torch.Tensor, beta: float = 1) -> 
             / (beta_squared * c_precision + c_recall)
         ).item()
     return 0.0
-
 
 
 def optimize_thresholds(
