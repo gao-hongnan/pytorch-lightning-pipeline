c3ff61fabfb4feb3562c6a184508bbbc059acf08
gaohn <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2023-02-18 12:31:24 +0800

    refactor: update configs

--- src/metrics/pf1.py
+++ src/metrics/pf1.py
@@ -9,23 +9,68 @@ import torch
 from torchmetrics import Metric
 
 
+import torch
+from torchmetrics import Metric
+
+
 class BinaryPFBeta(Metric):
-    def __init__(self, beta: float = 1, dist_sync_on_step=False):
+    """
+    PyTorch Metric that computes the Probabilistic F-beta score for binary classification.
+
+    Args:
+        beta: Float value of beta parameter in F-beta score. Default is 1.
+        dist_sync_on_step: Synchronize metric state across processes at each forward pass. Default is False.
+
+    Shape:
+        - Preds: (N, ) or (N, C)
+        - Labels: (N, ) where each value is either 0 or 1
+
+    Example:
+        >>> import torch
+        >>> from torchmetrics import PFBeta
+        >>> preds = torch.tensor([0.8, 0.6, 0.3, 0.2])
+        >>> labels = torch.tensor([1, 0, 1, 0])
+        >>> metric = PFBeta(beta=0.5)
+        >>> metric(preds, labels)
+        tensor(0.4732)
+
+    Reference:
+        [1] Powers, David M. "Evaluation: from precision, recall and F-measure to ROC, informedness, markedness & correlation."
+            Journal of machine learning technologies 2.1 (2011): 37-63.
+    """
+
+    def __init__(self, beta: float = 1, dist_sync_on_step: bool = False):
         super().__init__(dist_sync_on_step=dist_sync_on_step)
         self.beta = beta
         self.add_state("ctp", default=torch.tensor(0), dist_reduce_fx="sum")
         self.add_state("cfp", default=torch.tensor(0), dist_reduce_fx="sum")
         self.add_state("y_true_count", default=torch.tensor(0), dist_reduce_fx="sum")
 
-    def update(self, preds: torch.Tensor, target: torch.Tensor):
+    def update(self, preds: torch.Tensor, labels: torch.Tensor):
+        """
+        Update the state variables for computing PFBeta.
+
+        Args:
+            preds: Predicted probabilities of shape (N, ) or (N, C).
+            labels: Ground truth binary labels of shape (N, ).
+
+        Returns:
+            None
+        """
         if preds.ndim == 2:
             preds = preds[:, 1]
         preds = preds.clip(0, 1)
-        self.y_true_count += target.sum()
-        self.ctp += preds[target == 1].sum()
-        self.cfp += preds[target == 0].sum()
+        self.y_true_count += labels.sum()
+        self.ctp += preds[labels == 1].sum()
+        self.cfp += preds[labels == 0].sum()
 
-    def compute(self):
+    def compute(self) -> float:
+        """
+        Compute the PFBeta score.
+
+        Returns:
+            The computed PFBeta score as a float.
+        """
         c_precision = self.ctp / (self.ctp + self.cfp)
         c_recall = self.ctp / self.y_true_count
         beta_squared = self.beta ** 2
@@ -39,6 +84,12 @@ class BinaryPFBeta(Metric):
         return 0.0
 
     def reset(self):
+        """
+        Reset the state variables.
+
+        Returns:
+            None
+        """
         self.ctp = torch.tensor(0)
         self.cfp = torch.tensor(0)
         self.y_true_count = torch.tensor(0)
